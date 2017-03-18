c0873f72099584bd33d54f4e530c52149940dde5
森重翔平 <morishigeshouhei@morishigeshouheinoMacBook-Pro.local>
2017-03-18 22:42:43 +1300

    Initial Commit

---
+++ gamenheni/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gamenheni
+//
+//  Created by 森重翔平 on 2017/03/18.
+//  Copyright © 2017年 森重翔平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
